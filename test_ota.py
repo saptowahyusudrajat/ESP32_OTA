a88abda7d073767d9f963ece7823fd8f3ae44a0a
Sapto Wahyu Sudrajat <129891157+saptowahyusudrajat@users.noreply.github.com>
2025-06-17 14:35:44 +0700

    Update test_ota.py

--- test_ota.py
+++ test_ota.py
@@ -2,9 +2,9 @@ from machine import Pin
 from time import sleep
 p5 = Pin(5, Pin.OUT)
 
-while True:
+def loop():
   p5.value(0)
   sleep(1)
   p5.value(1)
   sleep(1)
-  print("Hello sapto lagi")
+  print("Hello i")
